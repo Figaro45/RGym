14aeb05a8a105efe003b26b78e58dc1a64cbe37a
NP <167005471+Figaro45@users.noreply.github.com>
2025-03-31 21:02:01 +0300

    Добавил swiper

---
+++ js/swiper.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    breakpoints: {
+        1500: {
+            slidesPerView: 1,
+        },
+        1300: {
+            slidesPerView: 0.9,
+        },
+        1200: {
+            slidesPerView: 0.8,
+        },
+        1100: {
+            slidesPerView: 0.7,
+        },
+        1000: {
+            slidesPerView: 0.6,
+        },
+        900: {
+            slidesPerView: 0.7,
+        },
+        800: {
+            slidesPerView: 0.6,
+        },
+        600: {
+            slidesPerView: 0.5,
+        },
+        400: {
+            slidesPerView: 0.4,
+        },
+        200: {
+            slidesPerView: 0.3,
+        },
+    }
+});
